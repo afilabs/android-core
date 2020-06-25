2ebec5b042da7e2f412df69fea6948201fdf656d
quangpv <quangpv.uit@gmail.com>
2020-06-25 19:56:35 +0700

    release core

---
+++ app/src/main/java/com/logistic/androidcore/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.logistic.androidcore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
